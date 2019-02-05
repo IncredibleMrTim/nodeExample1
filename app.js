17d78fa3b33c15d9c3a97d8c9676844630524f5a
Smart <smarti01@cpgorillas-mbp.risk.regn.net>
2019-02-05 15:39:23 +0000

    Added mongoDb connection

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-const http = require("http");
-const url = require("url");
-const module1 = require("./module1");
-
-const hostname = "127.0.0.1";
+const http = require('http');
+const url = require('url');
+const module1 = require('./module1');
+const MongoClient = require('mongodb').MongoClient;
+const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  var q = url.parse(req.url, true).query;
+// USE MONGO DB
 
-  res.writeHead(200, { "Content-Type": "application/json" });
+(async () => {
+	try {
+		const db = await MongoClient.connect(
+			'mongodb+srv://dbuser:TestTu8e@timmynet-zojxp.mongodb.net/test?retryWrites=true',
+			{ useNewUrlParser: true }
+		);
+		console.log('We are connected');
+		var dbo = db.db('test');
+		var query = {};
 
-  if (req.url.indexOf("user") > -1) {
-    res.end(JSON.stringify(module1.getUser(q.user)));
-  } else if (req.url.indexOf("season") > -1) {
-    res.end(JSON.stringify(module1.getSeason(q.season)));
-  }
-});
+		dbo.collection('testCollection')
+			.find()
+			.toArray((err, result) => {
+				console.log(result);
+			});
 
-server.listen(port, hostname, () => {
-  console.log(`sever running at http://${hostname}:${port}/`);
-});
+		db.close();
+	} catch (error) {
+		console.log(error);
+	}
+})();
+
+// USE CLASSIC Http.CreateServer
+
+// const server = http.createServer((req, res) => {
+//   var q = url.parse(req.url, true).query;
+
+//   res.writeHead(200, { "Content-Type": "application/json" });
+
+//   if (req.url.indexOf("user") > -1) {
+//     res.end(JSON.stringify(module1.getUser(q.user)));
+//   } else if (req.url.indexOf("season") > -1) {
+//     res.end(JSON.stringify(module1.getSeason(q.season)));
+//   }
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`sever running at http://${hostname}:${port}/`);
+// });
